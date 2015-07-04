2034c4951dabf7413987de0613f1abed2d8f6242
vidy <120648590@qq.com>
2015-07-04 16:21:56 +0800

    first upload

---
+++ addReady.js
@@ -0,0 +1,8 @@
+function addReady(fn){
+	if(document.addEventListener){
+		document.addEventListener('DOMContentLoaded',fn,false);
+	}else{
+		document.attachEvent('onreadystatechange',fn)
+	}
+}
+!window.$ && window.$ = addReady;
